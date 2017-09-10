36cc3f11ddf19ce9efe4cce1c7089e233037a435
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-09-10 15:46:46 -0400

    activacion de slick

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+$('.slider.mini-banner').slick({
+    infinite: true,
+    slidesToShow: 1,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+$('.slider.wide').slick({
+    infinite: true,
+    slidesToShow: 1,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
